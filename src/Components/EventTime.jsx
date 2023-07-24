8b54ddc50ddde9ae91841661da1607901e808f6b
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-07-24 11:16:25 +0530

    Event Time component added

--- src/Components/EventTime.jsx
+++ src/Components/EventTime.jsx
@@ -1,7 +1,7 @@
 import "../Styles/EventTime.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { EventCard } from "./EventCard";
-
+import { Box } from "@mui/material";
 const events = [
   {
     title: "India is growing",
@@ -17,8 +17,54 @@ const events = [
   },
 ];
 
+let data = [
+  {
+    title: "NASDAQ is ATH",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-24T10:38:00.000Z"),
+  },
+  {
+    title: "IT Market is booming again",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-24T10:38:00.000Z"),
+  },
+  {
+    title: "DB is leading FOrex Market",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-24T10:38:00.000Z"),
+  },
+  {
+    title: "GS annaounced major deal MS",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-24T10:38:00.000Z"),
+  },
+  {
+    title: "DB is leading FOrex Market",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-21T9:38:00.000Z"),
+  },
+  {
+    title: "DB is leading Forex Market",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-25T12:38:00.000Z"),
+  },
+  {
+    title: "GS annaounced major deal MS",
+    status: "ANNOUNCED",
+    action: "Summary",
+    time: new Date("2023-07-23T11:38:00.000Z"),
+  },
+];
+
 const EventTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const timeMoveRef = useRef(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -30,6 +76,38 @@ const EventTime = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const firstEventTime = data.length > 0 ? data[0].time.getTime() : null;
+    const currentTimeMs = currentTime.getTime();
+    const timeMoveElement = timeMoveRef.current;
+
+    if (firstEventTime && timeMoveElement) {
+      const pastDuration = currentTimeMs - firstEventTime;
+      const futureDuration = 24 * 60 * 60 * 1000 - pastDuration; // 24 hours in milliseconds
+
+      const pastPercentage = Math.min(
+        (pastDuration / (24 * 60 * 60 * 1000)) * 100,
+        100
+      );
+      const futurePercentage = Math.min(
+        (futureDuration / (24 * 60 * 60 * 1000)) * 100,
+        100
+      );
+
+      // console.info(
+      //   "====SHASHI=====",
+      //   pastDuration,
+      //   futureDuration,
+      //   pastPercentage,
+      //   futurePercentage
+      // );
+
+      timeMoveElement.style.backgroundSize = `${
+        pastPercentage + futurePercentage
+      }% auto`;
+    }
+  }, [currentTime]);
+
   const isPast = (time) => {
     return time < currentTime;
   };
@@ -38,39 +116,53 @@ const EventTime = () => {
     return time > currentTime;
   };
 
-  return (
-    <div>
-      <div className="time-container">
-        {[...Array(24).keys()].map((hour) => {
-          const time = new Date();
-          time.setHours(hour, 0, 0, 0);
-
-          let className = "time-slot";
-          if (isPast(time)) {
-            className += " past";
-          } else if (isFuture(time)) {
-            className += " future";
-          } else {
-            className += " present";
-          }
-
-          const eventsForHour = events.filter((event) => {
-            return event.time.getHours() === hour;
-          });
-
-          return (
-            <div key={hour} className={className}>
-              <div className="hour">{hour}:00</div>
-              {eventsForHour.map((eventDetails, index) => {
-                return (
-                  <EventCard event={eventDetails} key={index} showTimeAndDate />
-                );
-              })}
-            </div>
+  const renderData = (eventTime) => {
+    return data.map((ele, index) => {
+      return eventTime === "past"
+        ? isPast(ele.time) && (
+            <EventCard event={ele} key={index} showTimeAndDate />
+          )
+        : isFuture(ele.time) && (
+            <EventCard event={ele} key={index} showTimeAndDate />
           );
-        })}
-      </div>
-    </div>
+    });
+  };
+
+  return (
+    <Box>
+      <Box
+        ref={timeMoveRef}
+        className="time-move"
+        style={{ display: "flex", backgroundSize: "100% auto" }} // Adjust the background size as needed
+      >
+        <Box
+          className="time-slot past"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            padding: "1rem",
+          }}
+        >
+          {renderData("past")}
+        </Box>
+        <Box
+          className="time-slot future"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            padding: "1rem",
+          }}
+        >
+          {renderData("future")}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
