48bbc68c90ee34b6bddf7b878b780b03efe582e0
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-07-24 15:55:44 +0530

    Dashbaord file updated

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -26,56 +26,45 @@ const Dashboard = () => {
   })
 
   return (
-    <div style={{ display: 'flex' }}>
-      <DashboardContainer maxWidth='xl'>
-        <Grid container>
-          <Grid item xs={2}>
-            <Navbar />
-          </Grid>
-          <Grid item xs={2}>
-            <WidgetNavbar />
-          </Grid>
-          <Grid
-            container
-            direction='row'
-            spacing={1}
-            sx={{ marginTop: '90px' }}
-          >
-            <Grid item xs={12} sm={12} md={12}>
-              <Grid item>
-                <Widget>
-                  {!widget.tradingView ? (
-                    <TradingViewWidget />
-                  ) : (
-                    <WidgetItems setWidget={setWidget} />
-                  )}
-                </Widget>
-              </Grid>
-              <Grid item>
-                <Widget>
-                  {!widget.sentimentSignal ? <LineCharts /> : <WidgetItems />}
-                </Widget>
-              </Grid>
-              <Grid item>
-                <Widget>
-                  {!widget.mediaSignal ? (
-                    <CategoriesBarChart />
-                  ) : (
-                    <WidgetItems />
-                  )}
-                </Widget>
-              </Grid>
-              <Grid item>
-                <Widget>
-                  {!widget.eventDashboard ? <EventTime /> : <WidgetItems />}
-                  {/* <EventDashboard date={new Date()} events={events} /> */}
-                </Widget>
-              </Grid>
+    <DashboardContainer maxWidth='xl'>
+      <Grid container>
+        <Grid item xs={2}>
+          <Navbar />
+        </Grid>
+        <Grid item xs={2}>
+          <WidgetNavbar />
+        </Grid>
+        <Grid container direction='row' spacing={1} sx={{ marginTop: '90px' }}>
+          <Grid item xs={12} sm={12} md={12}>
+            <Grid item>
+              <Widget>
+                {!widget.tradingView ? (
+                  <TradingViewWidget />
+                ) : (
+                  <WidgetItems setWidget={setWidget} />
+                )}
+              </Widget>
+            </Grid>
+            <Grid item>
+              <Widget>
+                {!widget.sentimentSignal ? <LineCharts /> : <WidgetItems />}
+              </Widget>
+            </Grid>
+            <Grid item>
+              <Widget>
+                {!widget.mediaSignal ? <CategoriesBarChart /> : <WidgetItems />}
+              </Widget>
+            </Grid>
+            <Grid item>
+              <Widget>
+                {!widget.eventDashboard ? <EventTime /> : <WidgetItems />}
+                {/* <EventDashboard date={new Date()} events={events} /> */}
+              </Widget>
             </Grid>
           </Grid>
         </Grid>
-      </DashboardContainer>
-    </div>
+      </Grid>
+    </DashboardContainer>
   )
 }
 
