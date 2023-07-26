9d8b12d191904105b02983bd4aa59095e75ead4b
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-07-26 17:11:34 +0530

    Event country added

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -5,30 +5,30 @@ import {
   CardActions,
   CardContent,
   Typography,
-} from "@mui/material";
-import React from "react";
+} from '@mui/material'
+import React from 'react'
 
 export const EventCard = ({ event }) => {
   return (
     <Card
       sx={{
         p: 0,
-        width: "190px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        marginTop: "1rem",
+        width: '190px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        marginTop: '1rem',
       }}
     >
       <CardActionArea>
         <CardContent sx={{ p: 1, pb: 0.5 }}>
           <Typography
             sx={{
-              font: "IBM Plex Sans",
-              fontSize: "12px",
+              font: 'IBM Plex Sans',
+              fontSize: '12px',
               fontWeight: 700,
-              textOverflow: "ellipsis",
-              maxHeight: "30px",
+              textOverflow: 'ellipsis',
+              maxHeight: '30px',
             }}
           >
             {event.title}
@@ -36,23 +36,23 @@ export const EventCard = ({ event }) => {
         </CardContent>
       </CardActionArea>
       <CardActions sx={{ p: 1, pt: 0.5, pb: 0.2 }}>
-        <Typography sx={{ font: "IBM Plex Sans", fontSize: "10px", pt: 0.2 }}>
-          {event.status} |{" "}
+        <Typography sx={{ font: 'IBM Plex Sans', fontSize: '10px', pt: 0.2 }}>
+          {event.country} |{' '}
         </Typography>
         <Button
-          size="small"
+          size='small'
           sx={{
-            font: "IBM Plex Sans",
-            color: "#000AFF",
-            fontSize: "10px",
+            font: 'IBM Plex Sans',
+            color: '#000AFF',
+            fontSize: '10px',
             p: 0.5,
             pb: 0.2,
-            minWidth: "20px",
+            minWidth: '20px',
           }}
         >
           {event.action}
         </Button>
       </CardActions>
     </Card>
-  );
-};
+  )
+}
