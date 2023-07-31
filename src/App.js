342f9b5e012f7b1b850ddf3123cfce68298254c3
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-07-31 12:40:27 +0530

    Fetch currecnies added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Dashboard from './Pages/Dashboard'
-import LoginForm from './Pages/login'
-import ProtectedRoute from './Components/ProtectedRoute'
+// import LoginForm from './Pages/login'
+// import ProtectedRoute from './Components/ProtectedRoute'
+import { useEffect } from 'react'
+import { fetchCurrenciesRequest } from './Redux/actions/currenciesActions'
 
-function App() {
-  const appStyle = {
-    height: '100vh',
-  }
+const App = ({ fetchCurrencies }) => {
+  useEffect(() => {
+    fetchCurrencies()
+  }, [])
   return (
     <Router>
       <Routes>
@@ -19,4 +22,14 @@ function App() {
   )
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  currencies: state.currencies?.data,
+  loading: state.widgetsBar.loading,
+  error: state.widgetsBar.error,
+})
+
+const mapDispatchToProps = {
+  fetchCurrencies: fetchCurrenciesRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
